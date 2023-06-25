d701b212c0054d8cb55be0907436072804e39b52
R1nge <r1ngegamedev@gmail.com>
2023-06-25 10:49:54 +0300

    Init

---
+++ Assets/Scripts/GameFlow/GameplayScope.cs
@@ -0,0 +1,13 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+namespace GameFlow
+{
+    public class GameplayScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.RegisterEntryPoint<EntryPoint>().AsSelf();
+        }
+    }
+}
